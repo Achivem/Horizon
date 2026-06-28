8abbaa4d375182457c39c3606c758c58f61551e2
Achivem <achivem42@gmail.com>
2026-06-27 23:55:39 -0300

    Fixes

--- HorizonFront/src/pages/AdminPage.jsx
+++ HorizonFront/src/pages/AdminPage.jsx
@@ -1,16 +1,10 @@
 import "../App.css";
 import Navbar from "../components/Navbar";
-import NumberCard from "../components/NumberCard";
 
 function AdminPage() {
   return (
     <div className="admin-page">
       <Navbar />
-      <div className="number-cards-container">
-        <NumberCard title="Total Users" value="1,234" icon={null} />
-        <NumberCard title="Active Sessions" value="567" icon={null} />
-        <NumberCard title="System Status" value="Online" icon={null} />
-      </div>
     </div>
   );
 }
